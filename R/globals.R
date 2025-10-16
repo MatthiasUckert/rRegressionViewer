27752c2d0f05a2835ed6ec2ffa4a2f904b29c35c
MatthiasUckert <matthias.uckert@outlook.com>
2025-10-16 09:45:35 +0200

    Update

---
+++ R/globals.R
@@ -0,0 +1,21 @@
+#' Global Variables Declaration
+#'
+#' Declares global variables used in NSE (non-standard evaluation) contexts
+#' to avoid R CMD check NOTEs
+#'
+#' @keywords internal
+utils::globalVariables(c(
+  # From extract_coefficients function
+  "value",
+  "coef",
+  "stars",
+  
+  # From prepare_plot_data function
+  "Variable",
+  "Estimate",
+  "Stars",
+  "sign",
+  
+  # From create_significance_plot function
+  "abs_estimate"
+))
